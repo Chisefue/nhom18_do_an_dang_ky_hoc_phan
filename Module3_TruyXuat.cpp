a14ab87dc41423a94a791388b848f8e80ff3587c
Rinner2606 <tientm2606@gmail.com>
2026-09-22 10:12:43 +0700

    sua ten

--- Module3_TruyXuat.cpp
+++ Module3_TruyXuat.cpp
@@ -1,6 +1,12 @@
-#include "Module3_TruyXuat.h"
 
-// Kiểm tra định dạng: 0 < độ dài <= 13 và có dạng MaHocPhan_SoThuTuLop
+#include "Class.h"
+
+Course::Course(string maHP, string tenHP, string thoiGian) {
+    CourseID = maHP;
+    CourseName = tenHP;
+    Schedule = thoiGian;
+} 
+// Kiểm tra định dạng: 0 < độ dà i <= 13 và có dạng MaHocPhan_SoThuTuLop
 bool kiemTraDinhDang(string s) {
     if (s.empty() || s.length() > 13) {
         return false;
@@ -21,10 +27,10 @@ bool kiemTraDinhDang(string s) {
     }
 
     return true;
-}
+} 
 
 // Truy xuất danh sách sinh viên theo mã lớp
-void truyXuatDanhSachLop(string s, const vector<LopHocPhan>& danhSachCacLop) {
+void truyXuatDanhSachLop(string s, const vector<Classroom>& danhSachCacLop) {
     if (!kiemTraDinhDang(s)) {
         cout << "Ma lop khong hop le! (Yeu cau: 0 < do dai <= 13 va co dang MaHocPhan_SoThuTuLop)\n";
         return;
@@ -32,7 +38,7 @@ void truyXuatDanhSachLop(string s, const vector<LopHocPhan>& danhSachCacLop) {
 
     bool timThay = false;
     for (int i = 0; i < (int)danhSachCacLop.size(); i++) {
-        if (danhSachCacLop[i].maLop == s) {
+        if (danhSachCacLop[i].CLassID == s) {
             timThay = true;
             cout << "\n=== DANH SACH SINH VIEN LOP: " << s << " ===\n";
             if (danhSachCacLop[i].danhSachSV.empty()) {
