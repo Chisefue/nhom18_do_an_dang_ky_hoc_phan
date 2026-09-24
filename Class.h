d049d9d30ec0b9e8d3d18f9dc20c55b471b76f33
Chisefue <25110152@student.hcmute.edu.vn>
2026-09-24 09:24:20 +0700

    chỉnh format file header và thêm header guard

--- Class.h
+++ Class.h
@@ -1,3 +1,6 @@
+#ifndef _CLASS_H
+#define _CLASS_H
+
 #include <iostream>
 #include <string>
 #include <vector>
@@ -6,44 +9,46 @@
 using namespace std;
 
 class Course {
-private:
-    string courseId;
-    string courseName;
-    string schedule;
-public:
-    Course();
-    Course(string courseId, string courseName, string schedule);
+    private:
+        string courseId;
+        string courseName;
+        string schedule;
+    public:
+        Course();
+        Course(string courseId, string courseName, string schedule);
 
-    string getCourseId() const { return courseId; }
-    string getCourseName() const { return courseName; }
-    string getSchedule() const { return schedule; }
+        string getCourseId() const { return courseId; }
+        string getCourseName() const { return courseName; }
+        string getSchedule() const { return schedule; }
 };
 
 class Student {
-private:
-    string studentId;
-    string studentName;
-    Course course;
-    vector<Course> completedCourse;
-public:
-    Student();
-    Student(string studentId, string studentName, Course course);
+    private:
+        string studentId;
+        string studentName;
+        Course course;
+        vector<Course> completedCourse;
+    public:
+        Student();
+        Student(string studentId, string studentName, Course course);
 
-    string getStudentId() const { return studentId; }
-    string getStudentName() const { return studentName; }
-};
+        string getStudentId() const { return studentId; }
+        string getStudentName() const { return studentName; }
+    };
 
 class Classroom {
-private:
-    string classId;
-    string className;
-    vector<Student> studentList;
-    int capacity;
-public:
-    Classroom(string classId, string className, int capacity);
+    private:
+        string classId;
+        string className;
+        vector<Student> studentList;
+        int capacity;
+    public:
+        Classroom(string classId, string className, int capacity);
 
-    string getClassId() const { return classId; }
-    string getClassName() const { return className; }
-    const vector<Student>& getStudentList() const { return studentList; }
-    void addStudent(const Student& sv) { studentList.push_back(sv); }
-};
+        string getClassId() const { return classId; }
+        string getClassName() const { return className; }
+        const vector<Student>& getStudentList() const { return studentList; }
+        void addStudent(const Student& sv) { studentList.push_back(sv); }
+    };
+
+#endif
