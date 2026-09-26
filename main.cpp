3ce4ad1b55dfb2f327e0acc7d3e3521abca23600
Chisefue <25110152@student.hcmute.edu.vn>
2026-09-26 21:56:11 +0700

    Test thử chương module3

--- main.cpp
+++ main.cpp
@@ -6,14 +6,20 @@ int main() {
     Course dstt = Course("MATH143001_01", "Đại số tuyến tính và cấu trúc đại số", "Thứ 2: 7->11; Thứ 4: 7->10");
     Course nmcntt = Course("INIT130185_02", "Nhập môn ngành CNTT", "Thứ 4: 1->4; Thứ 5: 7->11");
     Student hai = Student("25162028", "Ronaldo Bui");
+    Student chieu = Student("25110152", "Chieu Dep Trai");
+    Student khoa = Student("25110241", "Messi Khoa");
     hai.addAttendingCourse(gdqp);
+    chieu.addAttendingCourse(dstt);
+    khoa.addAttendingCourse(dstt);
     hai.addCompletedCourse(dstt);
     hai.addCompletedCourse(nmcntt);
     Classroom dsttClass = Classroom("MATH143001_01", "V503", 70);
     Classroom nmcnttClass = Classroom("GDQP110131_02", "A5-204", 73);
     dsttClass.addStudent(hai);
+    dsttClass.addStudent(chieu);
+    dsttClass.addStudent(khoa);
     nmcnttClass.addStudent(hai);
     vector<Classroom> classes = {dsttClass, nmcnttClass};
-    truyXuatDanhSachLop("dsst01", classes);
+    truyXuatDanhSachLop("MATH143001_01", classes);
     return 0;
 }
